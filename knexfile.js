36a06ac14d7f04365c3b25065c86731426d491f5
chris.j.carter <chris.j.carter91@gmail.com>
2019-07-31 17:03:05 -0600

    Added tests

--- knexfile.js
+++ knexfile.js
@@ -32,6 +32,20 @@ module.exports = {
     }
   },
 
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./migrations"
+    },
+    seeds: {
+      directory: "./seeds"
+    }
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
@@ -45,20 +59,4 @@ module.exports = {
   }
 };
 
-// production: {
-//   client: 'pg',
-//   connection: process.env.DATABASE_URL,
-//   useNullAsDefault: true,
-//   pool: {
-//     afterCreate: (conn, done) => {
-//       conn.run('PRAGMA foreign_keys = ON', done);
-//     },
-//   migrations: {
-//     directory: "./migrations"
-//   },
-//   seeds: {
-//     directory: './seeds'
-//   }
-// }
-// }
-// };
+
